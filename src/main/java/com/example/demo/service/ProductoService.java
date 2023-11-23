71a840f50b929af24d1c10b6c7a4287bc1070125
gianpeccia <giann.peccia@gmail.com>
2023-11-23 00:36:09 -0300

    avances 4

--- src/main/java/com/example/demo/service/ProductoService.java
+++ src/main/java/com/example/demo/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.example.demo.model.Producto;
@@ -9,4 +10,5 @@ public interface ProductoService {
     public Optional<Producto> get(Integer id);
     public void update(Producto producto);
     public void delete(Integer id);
+    public List<Producto> findAll();
 }
